cc81a9a418743085d6a111a280a4d9f8a40411aa
VishvaGIT-hub <vishvadev543@gmail.com>
2025-10-22 15:49:24 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'loop-dash-v1.0.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/main.css',
+  '/css/animations.css',
+  '/css/components.css',
+  '/css/responsive.css',
+  '/js/main.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
